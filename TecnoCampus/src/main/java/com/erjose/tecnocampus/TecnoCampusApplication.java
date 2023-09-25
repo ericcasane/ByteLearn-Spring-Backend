31b863944acb7fe23eba3fc97171d6e0d09198db
Eric Casañé Torras <eric.eurotete@gmail.com>
2023-09-25 13:17:27 +0200

    Spring Project Creation
    Dependences: Spring Web, Spring Data JPA & H2Database

---
+++ TecnoCampus/src/main/java/com/erjose/tecnocampus/TecnoCampusApplication.java
@@ -0,0 +1,13 @@
+package com.erjose.tecnocampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoCampusApplication.class, args);
+	}
+
+}
---
+++ TecnoCampus/src/test/java/com/erjose/tecnocampus/TecnoCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erjose.tecnocampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
