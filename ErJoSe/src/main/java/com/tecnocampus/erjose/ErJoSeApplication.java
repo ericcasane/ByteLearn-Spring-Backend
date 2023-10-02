3acaff501cf41af3a9d1d32029afa7ac4a84b44d
Sergio <sbenages@edu.tecnocampus.cat>
2023-10-02 12:12:14 +0200

    Reset

--- ErJoSe/src/main/java/com/tecnocampus/erjose/ErJoSeApplication.java
+++ ErJoSe/src/main/java/com/tecnocampus/erjose/ErJoSeApplication.java
@@ -1,4 +1,4 @@
-package com.erjose.erjose;
+package com.tecnocampus.erjose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ErJoSeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ErJoSeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ErJoSeApplication.class, args);
+	}
 
 }
